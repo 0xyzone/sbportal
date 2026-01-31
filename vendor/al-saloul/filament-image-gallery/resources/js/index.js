4f79a5ac4037e978566ba93fd8593c65e4187c93
0xyzone <58982560+0xyzone@users.noreply.github.com>
2026-01-31 18:06:41 +0545

    Delete vendor directory

--- vendor/al-saloul/filament-image-gallery/resources/js/index.js
+++
@@ -1,3 +0,0 @@
-// Image Gallery Plugin - Viewer.js initialization
-// This file can be used for additional JS functionality
-// The main Viewer.js initialization is done inline in the viewer-script.blade.php component
