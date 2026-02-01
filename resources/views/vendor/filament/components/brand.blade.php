f799e0f0a620c669252d176f12e8a02bcd91ab6d
0xyzone <58982560+0xyzone@users.noreply.github.com>
2026-02-01 21:02:55 +0545

    Show current panel ID in brand component
    Adds a styled span to display the current Filament panel ID in the brand.blade.php component for improved context in the UI.

--- resources/views/vendor/filament/components/brand.blade.php
+++ resources/views/vendor/filament/components/brand.blade.php
@@ -4,9 +4,10 @@
     <div>
         <img src="{{ asset('images/favicon.png') }}" alt="favicon" style="height: 4rem;"> 
     </div>
-    <p class="font-bold text-2xl text-gray-800 dark:text-gray-200 flex lg:text-3xl">
+    <p class="font-bold text-2xl text-gray-800 dark:text-gray-200 flex lg:text-3xl items-center">
         <span>Spelling</span>
         <span>Bee</span>
         <span style="color: #eab308" class="font-bold">Hive</span>
+        <span class="text-sm px-2 py-1 bg-amber-400 text-black ml-2 capitalize rounded-lg">{{ Filament\Facades\Filament::getCurrentPanel()?->getId() }}</span>
     </p>
 </div>
