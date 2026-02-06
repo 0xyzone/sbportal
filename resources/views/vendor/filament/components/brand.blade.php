7be124887275d2ad85a6332af56ecc43107b9f5e
0xyzone <58982560+0xyzone@users.noreply.github.com>
2026-02-06 16:24:54 +0545

    Update brand.blade.php

--- resources/views/vendor/filament/components/brand.blade.php
+++ resources/views/vendor/filament/components/brand.blade.php
@@ -1,14 +1,14 @@
 @vite('resources/css/app.css')
-<div class="flex items-center" style="gap: 10px;">
+<div class="flex items-center mt-8 lg:mt-0" style="gap: 10px;">
 
-    <div>
+    <div class="shrink-0">
         <img src="{{ asset('images/favicon.png') }}" alt="favicon" style="height: 4rem;"> 
     </div>
-    <p class="font-bold text-2xl text-gray-800 dark:text-gray-200 flex lg:text-3xl items-center">
+    <p class="font-bold text-2xl text-gray-800 dark:text-gray-200 flex lg:text-3xl items-center flex-wrap py-2">
         <span>Spelling</span>
         <span>Bee</span>
         <span style="color: #eab308" class="font-bold">Hive</span>
-        <span class="text-sm px-2 py-1 bg-amber-400 text-black ml-2 capitalize rounded-lg">
+        <span class="text-sm px-2 py-1 bg-amber-400 text-black lg:ml-2 capitalize rounded-lg">
             {{ Filament\Facades\Filament::getCurrentPanel()?->getId() }}
         </span>
     </p>
